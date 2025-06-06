68ff951eced685e46a39b861309a3276990b1dca
MarcusHiller <marcus.hiller.msg@gmail.com>
2025-06-06 10:07:34 +0200

    statusbar file img path adapted

---
+++ js_images_path/statusbar_img_path.js
@@ -0,0 +1,34 @@
+const statusbarImages = new StatusbarImages(
+    [
+        'img/7_statusbars/1_statusbar/2_statusbar_health/green/0.png',
+        'img/7_statusbars/1_statusbar/2_statusbar_health/green/20.png',
+        'img/7_statusbars/1_statusbar/2_statusbar_health/green/40.png',
+        'img/7_statusbars/1_statusbar/2_statusbar_health/green/60.png',
+        'img/7_statusbars/1_statusbar/2_statusbar_health/green/80.png',
+        'img/7_statusbars/1_statusbar/2_statusbar_health/green/100.png'
+    ],
+    [
+        'img/7_statusbars/2_statusbar_endboss/orange/orange/0.png',
+        'img/7_statusbars/2_statusbar_endboss/orange/orange/20.png',
+        'img/7_statusbars/2_statusbar_endboss/orange/orange/40.png',
+        'img/7_statusbars/2_statusbar_endboss/orange/orange/60.png',
+        'img/7_statusbars/2_statusbar_endboss/orange/orange/80.png',
+        'img/7_statusbars/2_statusbar_endboss/orange/orange/110.png',
+    ],
+    [
+        'img/7_statusbars/1_statusbar/1_statusbar_coin/blue/0.png',
+        'img/7_statusbars/1_statusbar/1_statusbar_coin/blue/20.png',
+        'img/7_statusbars/1_statusbar/1_statusbar_coin/blue/40.png',
+        'img/7_statusbars/1_statusbar/1_statusbar_coin/blue/60.png',
+        'img/7_statusbars/1_statusbar/1_statusbar_coin/blue/80.png',
+        'img/7_statusbars/1_statusbar/1_statusbar_coin/blue/100.png',
+    ],
+    [
+        'img/7_statusbars/1_statusbar/3_statusbar_bottle/blue/0.png',
+        'img/7_statusbars/1_statusbar/3_statusbar_bottle/blue/20.png',
+        'img/7_statusbars/1_statusbar/3_statusbar_bottle/blue/40.png',
+        'img/7_statusbars/1_statusbar/3_statusbar_bottle/blue/60.png',
+        'img/7_statusbars/1_statusbar/3_statusbar_bottle/blue/80.png',
+        'img/7_statusbars/1_statusbar/3_statusbar_bottle/blue/100.png',
+    ]
+);
