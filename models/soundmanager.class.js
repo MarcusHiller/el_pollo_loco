d5973a23bfe74c18a9dcb102044fe30ab6c965ab
MarcusHiller <marcus.hiller.msg@gmail.com>
2025-07-06 10:21:10 +0200

    classe soundmanager created

---
+++ models/soundmanager.class.js
@@ -0,0 +1,32 @@
+class SoundManager {
+
+    constructor() {
+        this.bgMusic = new Audio('');
+        this.bgMusic.loop = true;
+        this.bgMusic.volume = 0.4;
+        this.muted = false;
+    }
+
+    playBackgroundMusic() {
+        if (!this.muted) {
+            this.bgMusic.play().catch(e => console.warn('Autoplay blockiert:', e));
+        }
+    }
+
+
+    stopBackgroundMusic() {
+        this.bgMusic.pause();
+        this.bgMusic.currentTime = 0;
+    }
+
+
+    toggleMute() {
+        this.muted = !this.muted;
+        this.bgMusic.muted = this.muted;
+    }
+
+    
+    isMuted() {
+        return this.muted;
+    }
+}
