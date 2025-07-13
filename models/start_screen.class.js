fac0b4868589b6ddd47d9469a54e246323bc80a5
MarcusHiller <marcus.hiller.msg@gmail.com>
2025-07-13 14:30:52 +0200

    clean code startScreen

--- models/start_screen.class.js
+++ models/start_screen.class.js
@@ -1,4 +1,5 @@
 class StartScreen {
+    
     constructor() {
         this.background = new DrawableObject();
         this.background.x = 0;
@@ -6,7 +7,6 @@ class StartScreen {
         this.background.width = 720;
         this.background.height = 480;
         this.background.loadImage('img/9_intro_outro_screens/start/startscreen_1.png');
-
         this.buttons = [
             new Button({ x: 310, y: 190, width: 100, height: 100, text: 'Play', action: 'Play', imagePath: 'img/icons/play-solid-hell-gray.svg' }),
             new Button({ x: 665, y: 20, width: 35, height: 35, text: 'Info', action: 'Info', imagePath: 'img/icons/info-solid-hell-gray.svg' }),
@@ -17,22 +17,25 @@ class StartScreen {
 
     draw(ctx, showHelp) {
         this.background.draw(ctx);
-
         this.buttons.slice(0, this.buttons.length - 1).forEach(btn => btn.draw(ctx, btn));
-
         if (showHelp) {
-            ctx.fillStyle = 'rgba(242, 159, 81, 0.95)';
-            ctx.fillRect(110, 90, 500, 300);
-            ctx.fillStyle = '#fff';
-            ctx.font = '28px DynaPuff';
-            let lines = this.infoBox().split('\n');
-            let y = 170;
-            for(let line of lines) {
-                ctx.fillText(line.trim(), 150, y);
-                y += 50;
-            }
-            this.buttons[this.buttons.length - 1].draw(ctx);
+            this.helpBox();
         }
+    }
+
+
+    helpBox() {
+        ctx.fillStyle = 'rgba(242, 159, 81, 0.95)';
+        ctx.fillRect(110, 90, 500, 300);
+        ctx.fillStyle = '#fff';
+        ctx.font = '28px DynaPuff';
+        let lines = this.infoBox().split('\n');
+        let y = 170;
+        for (let line of lines) {
+            ctx.fillText(line.trim(), 150, y);
+            y += 50;
+        }
+        this.buttons[this.buttons.length - 1].draw(ctx);
     }
 
 
