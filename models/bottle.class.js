485f23f56437a6c26a7df24c4fed2468a3f8c897
MarcusHiller <marcus.hiller.msg@gmail.com>
2025-07-15 15:21:13 +0200

    JSDOCs for bottle class

--- models/bottle.class.js
+++ models/bottle.class.js
@@ -1,12 +1,30 @@
+/**
+ * Represents a collectible bottle object in the game.
+ * Inherits rendering functionality from DrawableObject.
+ */
 class Bottle extends DrawableObject {
 
+    /**
+     * Collision offset for fine-tuning the hitbox.
+     * @type {{top: number, left: number, right: number, bottom: number}}
+     */
     offset = {
             top: 5,
             left: 5,
             right: 5,
             bottom: 5
         };
+    
         
+    /**
+     * Creates a new bottle at a randomized horizontal position.
+     * @param {string} imagePath - Path to the bottle image.
+     * @param {number} x - Max range for random horizontal offset.
+     * @param {number} y - Vertical position.
+     * @param {number} height - Height of the object.
+     * @param {number} width - Width of the object.
+     * @param {boolean} collected - Boolean statement as to whether the bottle was collected.
+     */
     constructor(imagePath, x, y) {
         super().loadImage(imagePath);
         this.x = 300 + Math.random() * x;
@@ -14,6 +32,5 @@ class Bottle extends DrawableObject {
         this.height = 90;
         this.width = 90;
         this.collected = false;
-        this.object = 'bottle';
     }
 }
