3449d9c58bb3c5bec7488c6366074d525c180b76
MarcusHiller <marcus.hiller.msg@gmail.com>
2025-07-18 06:55:31 +0200

    JSDOC for screen_manager class

--- models/screen_manager.class.js
+++ models/screen_manager.class.js
@@ -1,7 +1,21 @@
+/**
+ * Manages and renders different UI screens (start, win, lose) based on the current game state.
+ */
 class ScreenManager {
 
+    /**
+     * Initializes the start screen, win screen, and lose screen.
+     * Each screen has its own background layers and image.
+     */
     constructor() {
         this.startScreen = new StartScreen();
+
+
+        /**
+         * Screen shown when the player wins the game.
+         * 
+         * @type {EndScreen}
+         */
         this.wonScreen = new EndScreen([
             'img/5_background/layers/air.png',
             'img/5_background/layers/3_third_layer/2.png',
@@ -9,6 +23,12 @@ class ScreenManager {
             'img/5_background/layers/1_first_layer/2.png',
             'img/You won, you lost/You won A.png'
         ]);
+
+
+        /**
+         * Screen shown when the player loses the game.
+         * @type {EndScreen}
+         */
         this.lostScreen = new EndScreen([
             'img/5_background/layers/air.png',
             'img/5_background/layers/3_third_layer/2.png',
@@ -19,6 +39,14 @@ class ScreenManager {
     }
 
 
+    
+    /**
+     * Draws the appropriate screen depending on the current game state.
+     * 
+     * @param {CanvasRenderingContext2D} ctx - The canvas rendering context.
+     * @param {string} gameState - The current state of the game ('start', 'end-won', 'end-lose').
+     * @param {boolean} showHelp - Whether the help screen should be shown on the start screen.
+     */
     draw(ctx, gameState, showHelp) {
         if (gameState === 'start') {
             this.startScreen.draw(ctx, showHelp);
@@ -30,6 +58,13 @@ class ScreenManager {
     }
 
 
+    /**
+     * Returns the set of buttons currently active based on the game state.
+     * 
+     * @param {string} gameState - The current state of the game ('start', 'end-won', 'end-lose').
+     * @param {boolean} showHelp - If true, only the help button is returned on the start screen.
+     * @returns {Object[]} Array of active button objects for the given state.
+     */
     getButtons(gameState, showHelp) {
         if (gameState === 'start') {
             return showHelp ? this.startScreen.buttons.slice(this.startScreen.buttons.length -1) : this.startScreen.buttons.slice(0, this.startScreen.buttons.length -1);
